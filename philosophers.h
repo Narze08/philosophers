4e1c94b22d570a59e25406ec8f0a5f5d0d875b22
Narjiss Zemzami <nazemzam@c3r6p1.1337.ma>
2024-09-05 01:19:58 +0100

    start of the project

---
+++ philosophers.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nazemzam <nazemzam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/28 08:47:16 by nazemzam          #+#    #+#             */
+/*   Updated: 2024/07/28 09:07:51 by nazemzam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <time.h>
+# include <pthread.h>
+
+
+typedef struct s_philo
+{
+    
+}       t_philo;
+
+
+
+#endif
