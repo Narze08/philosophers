42b0c38fa7f702373b0a2fa0ed0f0775e6841a14
Narze08 <narze0173@gmail.com>
2024-09-19 13:10:27 +0100

    to clone in cluster

---
+++ init.c
@@ -0,0 +1,6 @@
+#include "philosophers.h"
+
+get_init(t_program *program, t_philo *philo)
+{
+    
+}
--- philosophers.c
+++ philosophers.c
@@ -3,17 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nazemzam <nazemzam@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mac <mac@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 08:46:56 by nazemzam          #+#    #+#             */
-/*   Updated: 2024/09/05 01:06:02 by nazemzam         ###   ########.fr       */
+/*   Updated: 2024/09/12 16:11:13 by mac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int main(int ac, char av)
+int	is_num(char *str)
 {
-    if(ac != 5 || ac != 6)
-        write(2, "wrong argument\n", 15);
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int check_args(char **av)
+{
+    if(ft_atoi(av[1]) > MAX_PHILO || ft_atoi(av[1]) <= 0 || is_num(av[1]) == 1)
+        return (write(2, "INVALID PHILOSOPHERS NUMBER\n", 28), 1);
+    if (ft_atoi(av[2]) <= 0 || is_num(av[2]) == 1)
+        return (write(2, "INVALID TIME TO DIE\n", 20), 1);
+    if (ft_atoi(av[3]) <= 0 || is_num(av[3]) == 1)
+        return (write(2, "INVALID TIME TO EAT\n", 20), 1);
+    if (ft_atoi(av[4]) <= 0 || is_num(av[4]) == 1)
+        return (write(2, "INVALID TIME TO SLEEP\n", 22), 1);
+    return (0);
+}
+
+int main(int ac, char **av)
+{
+    t_program       program;
+    t_philo         philo[MAX_PHILO];
+    pthread_mutex_t fork[MAX_PHILO];
+    
+    if (ac != 5 && ac != 6)
+        return (write(2, "WRONG ARGUMENTS NUMBER\n", 22), 1);
+    if (check_args(av) == 1)
+        return (1);
+    get_init(&program, philo);
 }
